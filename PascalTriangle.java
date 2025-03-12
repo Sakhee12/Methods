4313bf84eb19fd37226a01b6b6264284e6b13589
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-03-12 22:56:18 +0530

    Add files via upload

---
+++ PascalTriangle.java
@@ -0,0 +1,33 @@
+class PascalTriangle 
+{
+	public static void main(String[] args) 
+	{
+		pascalTri(8);
+	}
+
+	public static void pascalTri(int n)
+	{
+		for (int i=0;i<n;i++ )
+		{
+			for (int space=0;space<n-i-1;space++)
+			{
+				System.out.print(" "+" ");
+			}
+			for ( int star=0;star<=i ;star++ )
+			{
+				System.out.print(fact(i)/((fact(star))*fact(i-star))+"   ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static int fact(int num)
+	{
+		int fact=1;
+		for (int i=num;i>0 ;i-- )
+		{
+			fact*=i;
+		}
+		return fact;
+	}
+}
