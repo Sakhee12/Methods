d400c2afe3ebf7457808595fc43d813716791f74
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-05-13 23:44:53 +0530

    Add files via upload

---
+++ FinancialApplication.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class FinancialApplication 
+{
+	public static void main(String[] args) 
+	{
+		Scanner jg = new Scanner(System.in);
+        int subtotal = jg.nextInt;
+		float gratuity = jg.nextFloat;
+		System.out.println("Enter the Subtotal "+subtotal+ "and a tip rate "+gratuity);
+		float tip = (
+
+
+	}
+}
