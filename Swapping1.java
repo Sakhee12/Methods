824b0cbdb4fac2b2fff8c52f30ba3f8cee1cd31d
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-05-27 21:51:38 +0530

    Add files via upload

---
+++ Swapping1.java
@@ -0,0 +1,23 @@
+class Swapping1
+{
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=20;
+
+		System.out.println("Befor Swapping: ");
+		System.out.println("a: "+a);
+		System.out.println("b: "+b);
+
+		
+		a=b;
+		b=a;
+
+		System.out.println("After Swapping: ");
+		System.out.println("a: "+a);
+		System.out.println("b: "+b);
+
+
+
+	}
+}
