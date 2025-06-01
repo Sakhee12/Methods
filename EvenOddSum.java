982fe5671b7f3ba42a996af86904d6e10c0b0acd
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-06-01 22:07:21 +0530

    Add files via upload

---
+++ EvenOddSum.java
@@ -0,0 +1,28 @@
+class EvenOddSum 
+{
+	public static void main(String[] args) 
+	{
+		int num = 1234;
+        int evensum = 0;
+        int oddsum = 0;
+		int temp = 0;
+
+		int rem = num%10;
+		temp = (rem%2==0) ? (evensum=evensum+rem) : (oddsum=oddsum+rem);
+
+		num = num/10;
+		rem = num%10;
+		temp = (rem%2==0) ? (evensum=evensum+rem) : (oddsum=oddsum+rem);
+
+        num = num/10;
+		rem = num%10;
+		temp = (rem%2==0) ? (evensum=evensum+rem) : (oddsum=oddsum+rem);
+		
+		num = num/10;
+		rem = num%10;
+		temp = (rem%2==0) ? (evensum=evensum+rem) : (oddsum=oddsum+rem);
+
+		System.out.println("Even Sum "+evensum);
+		System.out.println("Odd Sum "+oddsum);
+	} 
+}
